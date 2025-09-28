c43bd07d85db716a4dd12fec0d300e66712e244b
Alberto Prada <albertojprada10@gmail.com>
2025-09-28 19:19:18 +0100

    validation

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const tasksController = require('../controllers/tasks');
-// const validation = require('../middleware/taskValidate');
+const validation = require('../middleware/taskValidate');
 const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', tasksController.getAll);
 router.get('/:id', tasksController.getSingle);
-router.post('/', isAuthenticated, tasksController.createTask);
-router.put('/:id', isAuthenticated, tasksController.updateTask);
+router.post('/', validation.saveTask, isAuthenticated, tasksController.createTask);
+router.put('/:id', validation.saveTask, isAuthenticated, tasksController.updateTask);
 router.delete('/:id', isAuthenticated,tasksController.deleteTask);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
-// const validation = require('../middleware/userValidate');
+const validation = require('../middleware/userValidate');
 const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
-router.post('/', isAuthenticated, usersController.createUser);
-router.put('/:id', isAuthenticated, usersController.updateUser);
+router.post('/',validation.saveUser, isAuthenticated, usersController.createUser);
+router.put('/:id',validation.saveUser, isAuthenticated, usersController.updateUser);
 router.delete('/:id', isAuthenticated, usersController.deleteUser);
 
-module.exports = router;    
+module.exports = router;
